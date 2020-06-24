56d9f70c64aed2c1fcc980ac3365e75ba245d5a7
unknown <wjdvlf9901@naver.com>
2020-06-25 02:01:01 +0900

    commit

--- test/src/main.js
+++ test/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.component('todo-item', {
+  template: '<li>This is a todo</li>'
+})
+
+var app = new Vue({
+  el: '#app'
+})
